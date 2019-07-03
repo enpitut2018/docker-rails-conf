2f5c812fa2d2b1c6e296c21d09ac0be8bd92597a
chiemi627 <chiemi@cs.tsukuba.ac.jp>
2019-07-03 23:13:10 +0900

    pairing/indexでgoogle spreadsheetのデータをもとにペアリングできるようにした

--- app/controllers/pairings_controller.rb
+++ app/controllers/pairings_controller.rb
@@ -4,33 +4,26 @@ require "google_drive"
 
 class PairingsController < ApplicationController
 
-  def pairing_with_mentors(participants,mentors)
-    pairs = []
+  def index
+    @data = {}
 
-    nonmentors_shfl = (participants - mentors).shuffle
-    mentors_shfl = mentors.shuffle
+    participants,mentors = readdata_from_google_spreadsheet("1TZCxBByvUiXRt71v8cQsVkdI356kAg19Gsh3U_OSs30")
+    @data[:pairs] = pairing_with_mentors(participants,mentors)    
+    session[:data] = @data      
+  end
 
-    [mentors_shfl.length,nonmentors_shfl.length].min.times {
-      pairs.push [nonmentors_shfl.shift,mentors_shfl.shift].sort
-    }
-    
-    rest = participants - pairs.flatten
+  def pair    
+    @data = {}
 
-    rest.shuffle.each_slice(2) do |x, y|
-      if y!=nil
-        pairs.push [x,y].sort
-      else
-        pairs.push [x,y]
+    if params[:file] 
+      if params[:file].content_type.downcase != "text/csv"
+        flash[:error] = "CSVファイルをアップロードしてください"
+        render
       end
+      participants,mentors = readdata_from_csv(params[:file].path)
+      @data[:pairs] = pairing_with_mentors(participants,mentors)    
     end
-    if rest.length % 2 == 1 then
-      pairs[-2].push pairs[-1][0]
-      pairs.pop
-    end
-    pairs.sort_by! { |pair|
-      pair[0]
-    }
-    return pairs
+    session[:data] = @data    
   end
 
   def save
@@ -61,7 +54,33 @@ class PairingsController < ApplicationController
     end
   end
 
-  def generate
+  def pairing_with_mentors(participants,mentors)
+    pairs = []
+
+    nonmentors_shfl = (participants - mentors).shuffle
+    mentors_shfl = mentors.shuffle
+
+    [mentors_shfl.length,nonmentors_shfl.length].min.times {
+      pairs.push [nonmentors_shfl.shift,mentors_shfl.shift].sort
+    }
+    
+    rest = participants - pairs.flatten
+
+    rest.shuffle.each_slice(2) do |x, y|
+      if y!=nil
+        pairs.push [x,y].sort
+      else
+        pairs.push [x,y]
+      end
+    end
+    if rest.length % 2 == 1 then
+      pairs[-2].push pairs[-1][0]
+      pairs.pop
+    end
+    pairs.sort_by! { |pair|
+      pair[0]
+    }
+    return pairs
   end
 
   def load_google_spreadsheet(key)
@@ -70,33 +89,17 @@ class PairingsController < ApplicationController
     sheets
   end
 
-  def pair    
-    @data = {}
-
-    if params[:file] 
-      if params[:file].content_type.downcase != "text/csv"
-        flash[:error] = "CSVファイルをアップロードしてください"
-        render
-      end
-      participants,mentors = readdata_from_csv(params[:file].path)
-      #participants,mentors = readdata_from_google_spreadsheet("1TZCxBByvUiXRt71v8cQsVkdI356kAg19Gsh3U_OSs30")
-      @data[:pairs] = pairing_with_mentors(participants,mentors)    
-    end
-    session[:data] = @data    
-  end
-
   def readdata_from_google_spreadsheet(key)
     participants = []
     mentors = []
     sheet = load_google_spreadsheet(key)
     (1..sheet.num_rows).each do |row|
-      next if row[0].blank?
-      id = row[0].to_i
+      next if sheet[row,1].blank?
+      id = sheet[row,1].to_i
       next if id==0 #to_iは不正な値を0にして返す。IDは1以上の整数なので0の場合はおかしな行と判定
-      puts id
-      unless row[2]=="1"
+      unless sheet[row,3]=="1"
         participants.push id
-        mentors.push id if row[1]=="1"
+        mentors.push id if sheet[row,2]=="1"
       end
     end      
     [participants,mentors]
@@ -109,7 +112,6 @@ class PairingsController < ApplicationController
       next if row[0].blank?
       id = row[0].to_i
       next if id==0 #to_iは不正な値を0にして返す。IDは1以上の整数なので0の場合はおかしな行と判定
-      puts id
       unless row[2]=="1"
         participants.push id
         mentors.push id if row[1]=="1"
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'pairings/pair'
   post 'pairings/pair'
+  get 'pairings/index'
+  post 'pairings/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
